5d53d9799129984a45369f80b382676df9f87114
Aitor <aitor.daw23@gmail.com>
2025-12-17 11:53:40 +0100

    landing page e imagenes

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com" rel="preconnect" />
+    <link crossorigin="" href="https://fonts.gstatic.com" rel="preconnect" />
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700;800&amp;display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&amp;display=swap" rel="stylesheet" />
+    <link href="{{asset('css/style.css')}}" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="d-flex flex-column min-vh-100">
+        
+        <header>@include('partials.header')</header>
+
+        <main>@yield('content')</main>
+
+        <footer>@include('partials.footer')</footer>
+
+    </div>
+</body>
+
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-md sticky-top shadow-sm px-4 py-3">
+      <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center gap-2" href="#">
+          <div class="d-flex align-items-center text-primary-color" style="width: 24px; height: 24px;">
+            <svg fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+              <path d="M6 6H42L36 24L42 42H6L12 24L6 6Z" fill="currentColor"></path>
+            </svg>
+          </div>
+          <h2 class="h5 mb-0 fw-bold">Azufit</h2>
+        </a>
+        <button aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-bs-target="#navbarNav" data-bs-toggle="collapse" type="button">
+          <span class="material-symbols-outlined">menu</span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-4 gap-md-2 text-center">
+            <li class="nav-item ">
+              <a class="nav-link fw-medium hover:text-primary-color" href="#">Clases Online</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fw-medium hover:text-primary-color" href="#">Clases Privadas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fw-medium hover:text-primary-color" href="#">Sobre Nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fw-medium hover:text-primary-color" href="#">Testimonios</a>
+            </li>
+          </ul>
+          <div class="d-flex justify-content-center align-items-center">
+            <button class="btn btn-primary rounded-lg px-4 py-2 fw-bold">
+              Regístrate
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
